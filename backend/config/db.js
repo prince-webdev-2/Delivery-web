8655ff3c541d5e1fda91f9d863f4b572807c016f
prince-webdev-2 <princewebdev2@gmail.com>
2025-08-22 22:59:54 +0530

    updated frontend code

--- backend/config/db.js
+++
@@ -1,4 +0,0 @@
-import mongoose from "mongoose";
-export const connectDB=async ()=>{
-        await mongoose.connect('mongodb+srv://prince:PRInce7870@cluster0.aoemjgi.mongodb.net/food-del').then(()=>console.log("DB Connected"))
-}
